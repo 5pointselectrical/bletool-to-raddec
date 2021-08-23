18dfb7e5620c34b3798abf964d3aa88365a8e949
rctweedy <89395040+rctweedy@users.noreply.github.com>
2021-08-23 17:11:54 -0400

    Update raddec.py
    Include updated packet and (still non-functioning) timestamp code.

--- raddec.py
+++ raddec.py
@@ -1,15 +1,15 @@
-#To-do: Get timestamp and packet code working properly
+#To-do: Get timestamp code working properly. It currently causes crashes with Pareto Anywhere.
 import sys
 import json
 import socket
 radflag="100"
 #timeflag="f00"
-#packflag="f10"
+packflag="f10"
 numrec="01"
-numpack="01"
+numpack="1"
 length="000"
 checkstring="00"
-IP_ADDRESS="[INSERT IP HERE]"
+IP_ADDRESS="[INSERT IP ADDRESS]"
 try:
    for line in iter(sys.stdin.readline, b''):
       dict=json.loads(line)
@@ -21,29 +21,30 @@ try:
       rectype=str('{:02x}'.format(dict['receiverIdType']))
       recid=dict['receiverId']
       rec=numrec+rssi+numdec+rectype+recid
-      #timestamp=str('{:x}'.format(dict['timestamp']))
-      #packet=dict['packets']
-      #packlength=str('{:03x}'.format(int(len(packet)/2)))
+      #timestamp=str('{:011x}'.format(dict['timestamp']))
+      packet=dict['packets']
+      packlength=str('{:02x}'.format(int(len(packet)/2)))
       base=radflag+length+beacon+rec
       #time=timeflag+timestamp
-      #data=packflag+numpack+packlength+packet
-      lengthint=int((len(base+checkstring))/2)
+      data=packflag+numpack+packlength+packet
+      lengthint=int((len(base+data+checkstring))/2)
       length=str('{:03x}'.format(lengthint))
       newbase=radflag+length+beacon+rec
       #timebase=newbase+time
-      #database=newbase+data
+      database=newbase+data
       #fullbase=newbase+time+data
       index=0
       checksum=0
       while index < int(lengthint - 1):
-            checksum +=int(newbase[index*2:index*2+2],16)
+            checksum +=int(database[index*2:index*2+2],16)
             index+=1
       checkstring=str('{:02x}'.format(checksum%256))
-      raddec=newbase+checkstring
-      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+      raddec=database+checkstring
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
       sock.sendto(bytes.fromhex(raddec), (IP_ADDRESS, 50001))
       print(raddec)
 except KeyboardInterrupt:
    sys.stdout.flush()
    pass
 print("\nGoodbye")
+
